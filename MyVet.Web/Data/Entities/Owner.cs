d8f1a9a06648a05d02737a6052fbfdbef9d3cbe2
DANNY ANDRES GAVIRIA VARGAS <dannygaviria207378@correo.itm.edu.co>
2020-02-11 12:56:25 -0500

    Add User Entities

---
+++ MyVet.Web/Data/Entities/Owner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class Owner
+    {
+        
+        public int Id { get; set; }
+
+        public User User { get; set; }
+
+        public ICollection<Pet> Pets { get; set; }
+
+        public ICollection<Agenda> Agendas { get; set; }
+
+
+
+
+
+
+
+    }
+}
